0dff6ecab51285c461ef9ceafc7c8b804fe03a74
OzoNeTT <gpoff12@mail.ru>
2020-04-11 04:00:56 +0300

    Here we go

--- logger.py
+++ logger.py
@@ -1,5 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.common.exceptions import NoSuchElementException
+
+# Setup settings
+def setUp(self):
+   self.setUpProfile()
+   self.setUpOptions()
+   self.setUpCapabilities()
+   self.setUpProxy()
+   self.driver = webdriver.Firefox(options=self.options, capabilities=self.capabilities, firefox_profile=self.profile)
+
+def setUpOptions(self):
+   self.options = webdriver.FirefoxOptions()
+   self.options.headless = self.headless
+
 
 def main():
 
@@ -13,7 +32,7 @@ def main():
     #TODO And here we go, reCaptcha bybass
 
     #press button
-    driver.find_element_by_class_name("btn").click()
+    driver.find_element_by_class_name("btna").click()
 
 
 if __name__ == "__main__":
