353a432d22dd31418bdf332f72d990c5350fd977
OzoNeTT <gpoff12@mail.ru>
2020-04-11 03:38:15 +0300

    Prepared for CB

---
+++ logger.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def main():
+
+    #open browser
+    driver = webdriver.Chrome()
+    driver.get("http://students.bmstu.ru")
+    #send login and pass
+    driver.find_element(By.NAME, 'username').send_keys("FFFFFF")
+    driver.find_element(By.NAME, 'password').send_keys("PPPPPP")
+
+    #TODO And here we go, reCaptcha bybass
+
+    #press button
+    driver.find_element_by_class_name("btn").click()
+
+
+if __name__ == "__main__":
+    main()
